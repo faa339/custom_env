c9f89ade8f583fecc2d296bb0215fc7d6f0a8789
Faris Alotaibi <faa339@nyu.edu>
2020-02-08 13:40:37 -0500

    Add args onto environ & display

--- env.c
+++ env.c
@@ -14,31 +14,49 @@ extern char **environ;
 #define ARRSIZE 80
 
 
-int getnumofVars(char** initialarglist);
-void addargstoArr(char** argsarr, char** initialarglist, int varcount);
+int GetNumofVars(char** initialarglist);
+void AddVarstoArr(char** argsarr, char** initialarglist, int varcount);
+void CopyArgstoEnviron(char** argsarr,int numofvars,int environsize);
+void DisplayEnv(void);
 
 int main(int argc, char *argv[])
 {
-	//If env is called alone, simply display the env vars
-	if(argc == 1)
+	if(argc==1)
 	{
-		while(*environ != 0)
-		{
-			printf("%s\n", *environ);
-			environ++;
-		}
+		DisplayEnv();
 		return 0;
+	}
+
+
+	//Getting the total # of ptrs in environ for later use
+	int environsize = 0;
+	char **environcounter = environ;
+	while(*environcounter != 0)
+	{
+		environsize++;
+		environcounter++;
 	}
 
 	//Array to store env vals from argv, loop to get them 
 	int varcount;
-	varcount = getnumofVars(argv);
+	varcount = GetNumofVars(argv);
 	char** argsarr = (char **) malloc(sizeof(char *) * varcount);
-	addargstoArr(argsarr,argv,varcount);
+	AddVarstoArr(argsarr,argv,varcount);
+	CopyArgstoEnviron(argsarr,varcount,environsize); 
+	DisplayEnv();
 	return 0;
-
 }
-int getnumofVars(char **initialarglist)
+
+void DisplayEnv(void)
+{
+	while(*environ!=0)
+	{
+		printf("%s\n",*environ);
+		environ++;
+	}
+}
+
+int GetNumofVars(char **initialarglist)
 {
 	int varnum = 0, i=0;
 	while(initialarglist[i] != 0)
@@ -55,12 +73,28 @@ int getnumofVars(char **initialarglist)
 	return varnum;
 }
 
-void addargstoArr(char** argsarr, char** initialarglist, int varcount)
+void AddVarstoArr(char** argsarr, char** initialarglist, int varcount)
 {
 	for(int i=1;i<=varcount;i++)
 	{
-		argsarr[i] =(char *) malloc(strlen(initialarglist[i])+1);
-		strcpy(argsarr[i],initialarglist[i]);
-		printf("%s\n",argsarr[i]);
+		argsarr[i-1] =(char *) malloc(strlen(initialarglist[i])+1);
+		strcpy(argsarr[i-1],initialarglist[i]);
 	}
+}
+
+void CopyArgstoEnviron(char** argsarr,int numofvars,int environsize)
+{
+	char **newenviron =(char**) malloc(sizeof(char*)*(environsize+numofvars));
+	for(int i=0;i<environsize;i++)
+	{
+		newenviron[i] = (char*) malloc(strlen(environ[i]) + 1);
+		strcpy(newenviron[i],environ[i]);
+	}
+
+	for(int j=environsize;j<(numofvars+environsize);j++)
+	{
+		newenviron[j] = (char*) malloc(strlen(argsarr[j]) + 1);
+		strcpy(newenviron[j],argsarr[(j-environsize)]);
+	}
+	environ = newenviron;
 }
